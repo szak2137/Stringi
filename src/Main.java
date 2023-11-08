d390de791d95bb704008c7b36acd99a543b856bd
szak2137 <szak97495@gmail.com>
2023-11-08 10:33:02 +0100

    wiecej stringow

--- src/Main.java
+++ src/Main.java
@@ -25,11 +25,33 @@ public class Main {
     String a = skan.nextLine();
     Scanner skan2 = new Scanner(System.in);
     String b = skan2.nextLine();
-    String con = new String();
-    if(a.length() > 2 && b.length() > 2){
-        con = con.concat(a.substring(0, 3));
-        con = con.concat(b.substring(0, 3));
+//    String con = new String();
+//    if(a.length() > 2 && b.length() > 2){
+//        con = con.concat(a.substring(0, 3));
+//        con = con.concat(b.substring(0, 3));
+//    }
+//        System.out.println(con);
+//
+//        String nowy = new String();
+//    int first = a.length();
+//    int second = b.length();
+//
+//    int min = first;
+//    if (min > second) min = second;
+//    for(int i = 0; i < a.length() && i < b.length(); i++ ){
+//       nowy = nowy.concat(a.substring(i, i+1));
+//       nowy = nowy.concat(b.substring(i, i+1));
+//    }
+//    if(first > second) nowy += a.substring(second, first);
+//    else nowy += b.substring(first, second);
+//    System.out.println(nowy);
+    // zad 3
+        if(a.length() > b.length()){
+            System.out.println(b+a+b);
+        }
+        else if(a.length() != b.length()){
+            System.out.println(a+b+a);
     }
-        System.out.println(con);
+
     }
 }
